ac27c7b2b9a428d37b28df9b0375d57a4de37396
Bryan Guevara <bryan19052000@gmail.com>
2021-10-08 22:05:12 -0700

    Completed part 3

--- labs/Lab3_BitManipulation/turnin/bguev006_lab3_part2.c
+++ labs/Lab3_BitManipulation/turnin/bguev006_lab3_part2.c
@@ -1,7 +1,7 @@
 /*	Author: Bryan Guevara
  *  Partner(s) Name: Bryan Guevara
  *	Lab Section:
- *	Assignment: Lab 3  Exercise 1
+ *	Assignment: Lab 3  Exercise 2
  *	Exercise Description: [optional - include for your own benefit]
  *
  *	I acknowledge all content contained herein, excluding template or example

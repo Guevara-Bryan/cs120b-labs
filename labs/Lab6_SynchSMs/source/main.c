bd3f7dec496daecfde5fa58b39c0e0d989bcc7b3
Bryan Guevara <bryan19052000@gmail.com>
2021-10-19 23:24:47 -0700

    Started lab 6

--- labs/Lab6_SynchSMs/source/main.c
+++ labs/Lab6_SynchSMs/source/main.c
@@ -1,29 +1,24 @@
 /*	Author: Bryan Guevara
- *  Partner(s) Name: Bryan Guevara
+ *  Partner(s) Name: 
  *	Lab Section:
- *	Assignment: Lab 5  Exercise 2
+ *	Assignment: Lab #  Exercise #
  *	Exercise Description: [optional - include for your own benefit]
+ *  Video Demo: 
  *
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
  */
 #include <avr/io.h>
-#include "io.h"
-
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
 #endif
 
 int main(void) {
     /* Insert DDR and PORT initializations */
-    DDRC = 0xFF; PORTC = 0x00;
-    DDRD = 0xFF; PORTD = 0x00;
+
     /* Insert your solution below */
+    while (1) {
 
-    LCD_init();
-
-    LCD_DisplayString(1, "hello potato");
-
-    while (1) { continue; }
+    }
     return 1;
 }

4b17319521120c8d7d109d5608b188c974ff940b
Bryan Guevara <bryan19052000@gmail.com>
2021-10-22 23:17:47 -0700

    Completed lab 7 part 2

--- labs/Lab6_SynchSMs/turnin/bguev006_lab6_part2.c
+++ labs/Lab6_SynchSMs/turnin/bguev006_lab6_part2.c
@@ -3,7 +3,7 @@
  *	Lab Section:
  *	Assignment: Lab 6  Exercise 2
  *	Exercise Description: [optional - include for your own benefit]
- *  Video Demo: https://youtu.be/k1jmEqx2m8U
+ *  Video Demo: 
  *
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
